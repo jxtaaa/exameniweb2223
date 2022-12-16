f5ab6e8927bfe9a6936f1c70502ce5855af0c44a
José Luis Bueno <jlbp@uma.es>
2022-12-16 13:25:15 +0100

    updated index

--- frontend/src/pages/index.ts
+++ frontend/src/pages/index.ts
@@ -1,5 +1,5 @@
 import type { APIContext } from "astro";
 
 export async function get(context: APIContext) {
-    return context.redirect("/examen?" + context.url.searchParams);
+    return context.redirect("/viviendas?" + context.url.searchParams);
   }
